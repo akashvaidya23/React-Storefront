3220ee44e9544eb86a67255086b78b6511499a5c
akashvaidya23 <akash.vaidya23@gmail.com>
2024-04-03 09:44:47 +0530

    Added products component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,43 @@
-// import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
-import { SfButton } from "@storefront-ui/react";
 import NavbarTop from "./Components/NavbarTop";
+import ProductCard from "./Components/ProductCard";
 
 function App() {
   // const [count, setCount] = useState(0);
+  const [products, setProducts] = useState([]);
+  const getProducts = async () => {
+    await fetch("https://dummyjson.com/products")
+      .then((res) => res.json())
+      .then((data) => {
+        // console.log(data.products);
+        setProducts(data.products);
+      });
+  };
+  useEffect(() => {
+    getProducts();
+    console.log("inside array");
+  }, []);
+  console.log(products);
 
   return (
     <>
       <NavbarTop />
-      {/* <p>Akash</p> */}
-      {/* <SfButton className="w-full">Hello</SfButton> */}
+      {/* {products.map((product) => {
+        return <p key={product.id}>{product.title}</p>;
+      })} */}
+      <div
+        style={{
+          display: "flex",
+          flexFlow: "row",
+          flexWrap: "wrap",
+          gap: "20px",
+          margin: "20px",
+        }}
+      >
+        <ProductCard />
+        <ProductCard />
+      </div>
     </>
   );
 }

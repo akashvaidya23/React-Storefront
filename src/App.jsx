0bafd9e0022878e5280253ec1e77cc15196a2463
akashvaidya23 <akash.vaidya23@gmail.com>
2024-04-03 09:06:41 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+// import { useState } from "react";
+import "./App.css";
+import { SfButton } from "@storefront-ui/react";
+import NavbarTop from "./Components/NavbarTop";
+
+function App() {
+  // const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <NavbarTop />
+      {/* <p>Akash</p> */}
+      {/* <SfButton className="w-full">Hello</SfButton> */}
+    </>
+  );
+}
+
+export default App;
